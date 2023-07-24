077132b71f6b29a1c75c46313a45ea01e7eb600f
amipei <156516399@qq.com>
2023-07-24 08:27:19 +0800

    first commit

---
+++ apps/blog/app/_nav/Nav.tsx
@@ -0,0 +1,10 @@
+export default () => {
+  return (
+    <aside className="w-[260px] bg-custom-primary">
+      <nav className="flex flex-col">
+        <a href="">首页</a>
+        <a href="">关于我</a>
+      </nav>
+    </aside>
+  )
+}
